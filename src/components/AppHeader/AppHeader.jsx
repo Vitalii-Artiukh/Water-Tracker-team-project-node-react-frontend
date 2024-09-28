91990afbbb62c48a822cbec21c458e1cca7458e6
StingStorm <alexkalganov18@gmail.com>
2024-09-28 19:23:07 +0300

    small base structure refactoring

---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,13 @@
+import css from './AppHeader.module.css';
+
+const Header = () => {
+  return (
+    <div className={css.header}>
+      <p>Kalhanov Oleksandr</p>
+      <p>Homework-2</p>
+      <p>React-108</p>
+    </div>
+  );
+};
+
+export default Header;
