9f1c5042532e0b598d59eccca6cb1eaa9f71e953
Vitalii Artiukh <radial_car@yahoo.com>
2025-02-02 15:56:29 +0200

    background

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,16 +1,13 @@
 import WaterConsumptionTracker from "../../components/WaterConsumptionTracker/WaterConsumptionTracker.jsx";
 import WhyDrinkWater from "../../components/WhyDrinkWater/WhyDrinkWater.jsx";
-import Container from "../../components/ui/Container/Container.jsx";
 import css from "./WelcomePage.module.css";
 
 const WelcomePage = () => {
   return (
-    <Container>
-      <div className={css.wrapper}>
-        <WaterConsumptionTracker />
-        <WhyDrinkWater />
-      </div>
-    </Container>
+    <div className={css.wrapper}>
+      <WaterConsumptionTracker />
+      <WhyDrinkWater />
+    </div>
   );
 };
 
