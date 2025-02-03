044ce3474dc8c191c790e0f3b888f68962ba67c3
Lena_Bondar <lencha.2005@gmail.com>
2025-02-03 16:28:51 +0200

    fix homePage css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,17 @@
 import DailyNorma from "../../components/DailyNorma/DailyNorma";
 import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
+
 import css from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <div className={css.container}>
+    <>
 <div className={css.imgWrap}></div>
       <DailyNorma />
       <WaterRatioPanel />
       {/* <p>Today Water List</p>
       <p>Month Stats Table</p> */}
-    
-    </div>
+    </>
   );
 };
 
