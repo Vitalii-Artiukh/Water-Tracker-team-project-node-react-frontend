5a3cf3b0c54463f5ce1dd91773e898ff5e18e76f
John_Konstantin <overdose.ut@gmail.com>
2025-02-03 21:32:57 +0200

    add my files

--- src/components/TodayWaterListItem/TodayWaterListItem.jsx
+++ src/components/TodayWaterListItem/TodayWaterListItem.jsx
@@ -1,7 +1,28 @@
+import Icon from "../ui/Icon";
 import css from "./TodayWaterListItem.module.css";
 
 const TodayWaterListItem = () => {
-  return <div className={css.item}>200 ml 14:00 PM</div>;
+  return (
+    <div className={css.item}>
+      <div className={css.waterStatsPanel}>
+        <svg className={css.svg}>
+          <use href={"/public/sprite.svg#icon-glass"}></use>
+        </svg>
+        <span className={css.itemTextWater}>200 ml</span>
+        <span className={css.itemTextTime}>14:00 PM</span>
+      </div>
+      <div className={css.iconsPanel}>
+        <svg width={16} height={16}>
+          <use href={"/public/symbol-defs3.svg#icon-pencil-square"}></use>
+        </svg>
+        <svg width={16} height={16}>
+          <use href={"/public/symbol-defs3.svg#icon-trash"}></use>
+        </svg>
+        {/* <Icon name={"icon-edit"} width={16} height={16} />
+        <Icon name={"icon-trash"} width={16} height={16} /> */}
+      </div>
+    </div>
+  );
 };
 
 export default TodayWaterListItem;
