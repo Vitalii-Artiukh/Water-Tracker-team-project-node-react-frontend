40ceb4084a29133aa327038cd631400ac1256ddd
Andrii <m9ta92@gmail.com>
2025-02-03 21:13:23 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,26 @@
+import css from "../Modals/SettingModal/SettingModal.module.css";
+import ModalContainer from "../ui/ModalContainer/ModalContainer";
+import SettingModal from "../Modals/SettingModal/SettingModal";
+import { useState } from "react";
+
 const Header = () => {
+  const [isSettingModalOpen, setIsSettingModalOpen] = useState(false);
+  const openSettingModal = () => setIsSettingModalOpen(true);
+  const closeSettingModal = () => setIsSettingModalOpen(false);
+
   return (
     <div>
       <span>LOGO</span>
+      <button onClick={openSettingModal} type="button">
+        Setting
+      </button>
+      <ModalContainer
+        overlayClassName={css.overlaySettingModal}
+        className={css.settingModalWrapper}
+        isOpen={isSettingModalOpen}
+        onClose={closeSettingModal}>
+        <SettingModal onClose={closeSettingModal} />
+      </ModalContainer>
     </div>
   );
 };
