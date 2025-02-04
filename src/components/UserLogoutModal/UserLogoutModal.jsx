d9f874a6828bf05aebaf2a6e6c50cfebb90e0638
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-04 23:52:09 +0200

    Add UserLogoutModal styles

--- src/components/UserLogoutModal/UserLogoutModal.jsx
+++ src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -1,11 +1,9 @@
 import { useDispatch } from "react-redux";
 import { logout } from "../../redux/auth/operations";
-import Modal from "react-modal";
+import ModalContainer from "../ui/ModalContainer/ModalContainer.jsx";
 import Icon from "../ui/Icon.jsx";
 import css from "./UserLogoutModal.module.css";
-import ModalContainer from "../ui/ModalContainer/ModalContainer.jsx";
-
-Modal.setAppElement("#root");
+import clsx from "clsx";
 
 const UserLogoutModal = ({ isOpen, onClose }) => {
   const dispatch = useDispatch();
@@ -17,18 +15,25 @@ const UserLogoutModal = ({ isOpen, onClose }) => {
 
   return (
     <ModalContainer isOpen={isOpen} onClose={onClose}>
-      <h2>Log out</h2>
-      <button className={css.closeBtn} onClick={onClose} aria-label="Close">
-        <Icon width={16} height={16} />
-      </button>
-      <p className={css.subtitle}>Do you really want to leave?</p>
-      <div className={css.buttons}>
-        <button onClick={handleLogout} className={css.logoutBtn}>
-          Log out
-        </button>
-        <button onClick={onClose} className={css.cancelBtn}>
-          Cancel
-        </button>
+      <div className={css.modalContent}>
+        <div className={css.modalHeader}>
+          <h2 className={css.title}>Log out</h2>
+          <button className={css.closeBtn} onClick={onClose} aria-label="Close">
+            <Icon name="icon-edit" width={24} height={24} />
+          </button>
+        </div>
+        <p className={css.subtitle}>Do you really want to leave?</p>
+        <div className={css.buttonGroup}>
+          <button
+            onClick={handleLogout}
+            className={clsx(css.button, css.logoutBtn)}
+          >
+            Log out
+          </button>
+          <button onClick={onClose} className={clsx(css.button, css.cancelBtn)}>
+            Cancel
+          </button>
+        </div>
       </div>
     </ModalContainer>
   );
