102140f67dfd4bec356f4587bb6c405c94745167
StingStorm <alexkalganov18@gmail.com>
2025-02-07 03:51:11 +0200

    water redux in progress

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -1,2 +1,29 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import api from '../../api/axiosInstance.js';
+
+export const fetchWaterEntriesByDay = createAsyncThunk(
+  'water/fetchAllByDay',
+  async (date, thunkAPI) => {
+    try {
+      const { data } = await api.get(`/water/day/${date}`);
+      console.log(data);
+
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addWaterEntrie = createAsyncThunk(
+  'water/addEntrie',
+  async (entrieData, thunkAPI) => {
+    try {
+      const { data } = api.post('/water', entrieData);
+
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
--- src/redux/water/slice.js
+++ src/redux/water/slice.js
@@ -1,19 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { addWaterEntrie, fetchWaterEntriesByDay } from './operations.js';
+import { getDayMonthDate } from '../../utils/dateUtils.js';
 
-const handlePending = state => {
-  state.loading = true;
-};
+// const handlePending = state => {
+//   state.loading = true;
+// };
 
-const handleRejected = state => {
-  state.loading = false;
-};
+// const handleRejected = state => {
+//   state.loading = false;
+// };
 
 const waterSlice = createSlice({
   name: 'water',
   initialState: {
-    items: [],
+    monthStats: [],
+    dailyRecords: {
+      date: null,
+      dailyNorm: 2000,
+      totalWater: null,
+      dailyNormProgress: null,
+      entires: [],
+    },
     currentServing: null,
-    loading: false,
+    loading: {
+      monthLoading: false,
+      dailyLoading: false,
+    },
+    error: null,
   },
 
   reducers: {
@@ -22,7 +35,51 @@ const waterSlice = createSlice({
     },
   },
 
-  extraReducers: builder => builder,
+  extraReducers: builder =>
+    builder
+      .addCase(fetchWaterEntriesByDay.fulfilled, (state, action) => {
+        state.dailyRecords = {
+          date: action.payload.date,
+          dailyNorm: action.payload.dailyNorm,
+          totalWater: action.payload.totalWater,
+          dailyNormProgress: action.payload.percentage,
+          entires: action.payload.entires,
+        };
+      })
+      .addCase(addWaterEntrie.fulfilled, (state, action) => {
+        if (state.dailyRecords.date === action.payload.date) {
+          state.dailyRecords = {
+            dailyNorm: action.payload.dailyNorm,
+            totalWater: action.payload.totalWater,
+            dailyNormProgress: action.payload.percentage,
+            entires: action.payload.entires,
+          };
+        }
+
+        const formattedDate = getDayMonthDate(action.payload.date);
+
+        const dayToUpdate = {
+          date: formattedDate,
+          dailyNorma: action.payload.dailyNorm,
+          percentage: `${
+            (action.payload.totalWater / action.payload.dailyNorm) * 100
+          }%`,
+          entryCount: action.payload.entires.length,
+          entries: action.payload.entires,
+        };
+
+        const isRecordExist = state.monthStats.some(
+          day => day.date === formattedDate
+        );
+
+        if (isRecordExist) {
+          state.monthStats = state.monthStats.map(day =>
+            day.date === formattedDate ? dayToUpdate : day
+          );
+        } else {
+          state.monthStats.push(dayToUpdate);
+        }
+      }),
 });
 
 export const { setCurrentServing } = waterSlice.actions;
