b4aedaeb0a9bbb35779907f5d0850f7762e40ca8
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-07 19:07:09 +0200

    Fix handleSubmit in signup

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,14 +1,20 @@
-import { useDispatch } from 'react-redux';
-import { signUp } from '../../redux/auth/operations.js';
-import AuthForm from '../../components/AuthForm/AuthForm';
+import { useDispatch } from "react-redux";
+import { signUp } from "../../redux/auth/operations.js";
+import AuthForm from "../../components/AuthForm/AuthForm";
 
 const SignupPage = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = (values, actions) => {
-    dispatch(signUp(values));
+  const handleSubmit = async (values) => {
+    const { confirmPassword, ...formData } = values;
 
-    actions.resetForm();
+    console.log("Форма отправлена", formData);
+    try {
+      await dispatch(signUp(formData)).unwrap();
+    } catch (error) {
+      console.error("Ошибка регистрации:", error);
+      alert(`Ошибка регистрации: ${error.message}`);
+    }
   };
 
   return <AuthForm title="Sign Up" type="signUp" onSubmit={handleSubmit} />;
