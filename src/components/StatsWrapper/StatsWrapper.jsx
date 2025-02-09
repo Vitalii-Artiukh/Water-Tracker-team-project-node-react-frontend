7621ad1a5753d930a3114669991a6e0f3d0669e5
EgorOgichuk <123835482+EgorOgirchuk@users.noreply.github.com>
2025-02-09 22:39:21 +0200

    feature/  water add edit model

--- src/components/StatsWrapper/StatsWrapper.jsx
+++ src/components/StatsWrapper/StatsWrapper.jsx
@@ -2,15 +2,11 @@ import MonthStatsTable from "../MonthStatsTable/MonthStatsTable";
 import TodayWaterList from "../TodayWaterList/TodayWaterList";
 import css from "./StatsWrapper.module.css";
 
-const StatsWrapper = ({dailyRecords, setWaterEntry, openWaterModal}) => {
+const StatsWrapper = (dailyRecords) => {
   return (
     <div className={css.container}>
-      <TodayWaterList
-        dailyRecords={dailyRecords}
-        setWaterEntry={setWaterEntry}
-        openWaterModal={openWaterModal}
-      />
-      <MonthStatsTable/>
+      <TodayWaterList dailyRecords={dailyRecords} />
+      <MonthStatsTable />
     </div>
   );
 };
