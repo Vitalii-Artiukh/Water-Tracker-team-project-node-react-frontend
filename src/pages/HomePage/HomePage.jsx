019a3579aa5da6c76200c370c88eab0d32c61ffd
StingStorm <alexkalganov18@gmail.com>
2025-02-09 16:30:32 +0200

    remove unness code from Home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 import DailyNorma from '../../components/DailyNorma/DailyNorma';
 import StatsWrapper from '../../components/StatsWrapper/StatsWrapper';
@@ -7,40 +6,15 @@ import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
 import MyDailyNormaModal from '../../components/MyDailyNormaModal/MyDailyNormaModal';
 
 import css from './HomePage.module.css';
-import { waterOperations } from '../../redux/index.js';
 
 const HomePage = () => {
-  const dispatch = useDispatch();
   const [isNormaModalOpen, setIsNormaModalOpen] = useState(false);
 
   const openModal = () => setIsNormaModalOpen(true);
   const closeModal = () => setIsNormaModalOpen(false);
 
-  useEffect(() => {
-    dispatch(waterOperations.fetchTodayWaterRecords());
-    dispatch(waterOperations.fetchWaterMonthStats('2025-02'));
-  }, [dispatch]);
-
   return (
     <div className={css.pageWrapper}>
-      <button
-        style={{
-          position: 'absolute',
-          top: '10%',
-          left: '40%',
-        }}
-        type="button"
-        onClick={() =>
-          dispatch(
-            waterOperations.addWaterEntrie({
-              time: '2024-02-09T02:30',
-              waterVolume: 200,
-            })
-          )
-        }
-      >
-        add water
-      </button>
       <MyDailyNormaModal isOpen={isNormaModalOpen} closeModal={closeModal} />
       <div>
         <DailyNorma openModal={openModal} />
